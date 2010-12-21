47cd41db520142e03a0ad2c9ca630e762902a4f9
jelle <jelle@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2010-12-21 16:39:33 +0000

    add tokens.py
    git-svn-id: http://dev.inghist.nl/svn/names/trunk@20255 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

---
+++ names/tests/test_tokens.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python    
+# encoding=utf8
+
+import unittest
+
+from names.tokens import *
+
+class TokensTestCase(unittest.TestCase):
+    """test methods from common.py"""
+    def test_tokens(self):
+        s = 'Abc d? 1 d223r! (xxx)  *  --- \nMercier-Camier'
+        t = tokens(s)
+#        print s
+#        print ''.join(['%s%s' % (word, tail) for word, tail in t])
+        self.assertEqual(''.join(['%s%s' % (word, tail) for word, tail in t]), s)
+        
+        self.assertEqual(tokens('H.P.'),[('H.', ''), ('P.', '')])
+def test_suite():
+    return unittest.TestSuite((
+        unittest.makeSuite(TokensTestCase),
+        ))
+
+if __name__=='__main__':
+    unittest.main()
+
+        
