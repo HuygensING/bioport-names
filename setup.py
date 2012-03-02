391b758db34b31315abdb43ee2dbd4184ee37a19
jelle <jelle@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2012-03-02 14:46:17 +0000

    git-svn-id: http://dev.inghist.nl/svn/names/trunk@25501 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name = 'names',
-      version = '0.3.1dev',
+      version = '0.3.2',
       packages = ['names'],
       install_requires=['python-Levenshtein',
                         'lxml',
